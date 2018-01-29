76b1933f44e81ee52338d6e2f1f7352a9ea35130
Sterling Hirsh <mybadluck22@gmail.com>
2018-01-28 18:29:38 -0600

    Change wording, add more phrases

--- src/ReadyView.js
+++ src/ReadyView.js
@@ -6,7 +6,7 @@ class ReadyView extends Component {
     <div className="readyView">
       {this.props.gameOver ? <h2>Game Over</h2> : ''}
       <h3>1. Speak the phrase.</h3>
-      <h4>2. Click the button when someone else says that phrase.</h4>
+      <h4>2. Click a button if someone else says the phrase on it.</h4>
       <button onClick={this.props.onReady}>Start</button>
       <h5>Best with 2+ players</h5>
     </div>
